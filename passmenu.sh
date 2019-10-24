76792ad3ab4999d5105633e482df3402f53e6d59
Nelson Healy <nelson@atom.industries>
2019-10-24 16:24:50 -0400

    Added comments for gpg commands and removed unneeded return statements

--- passmenu.sh
+++ passmenu.sh
@@ -5,13 +5,14 @@ path=$HOME/.password-store # Location of password store file
 passname=$(ls $path | cut -d '.' -f 1 | dmenu)
 
 function main {
+# If card present + unlocked or has had incorrect PIN tried
 if [[ $(gpg-connect-agent 'scd getinfo card_list' /bye | grep 'SERIALNO') ]]
 then
 	password=$(gpg --batch --pinentry-mode loopback -d $path/$passname.gpg | cut -d$'\n' -f $req_field)
 	if [[ $password ]]
 	then
 		type_password $password
-		return 1
+		return
 	fi
 fi
 unlock_questions
@@ -19,22 +20,20 @@ password=$(gpg --batch --pinentry-mode loopback --passphrase $passcode -d $path/
 if [[ $password ]]
 then
 	type_password $password
-	return 1
 fi
 }
 
 function unlock_questions {
+	# If card not inserted at all
 	if [[ ! $(gpg --card-status | grep 'Serial number') ]]
 	then
 		true | dmenu -p 'Insert smartcard and press enter...'
 	fi
 	passcode=$(true | dmenu -p "Input smartcard passcode")
-	return 1
 }
 
 function type_password {
 	echo "str" $1 | xte
-	return 1
 }
 
 main
