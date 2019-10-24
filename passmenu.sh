10f66fbd55279c6feddf1a8e217be0c591209615
Nelson Healy <nelson@atom.industries>
2019-10-24 16:40:39 -0400

    Added breakpoints to exit if password not selected or user c-c to exit program

--- passmenu.sh
+++ passmenu.sh
@@ -3,6 +3,7 @@
 req_field=${1-1} # Line of password file to type
 path=$HOME/.password-store # Location of password store file
 passname=$(ls $path | cut -d '.' -f 1 | dmenu)
+[[ -n $passname ]] || exit
 
 function main {
 # If card present + unlocked or has had incorrect PIN tried
@@ -28,6 +29,10 @@ function unlock_questions {
 	if [[ ! $(gpg --card-status | grep 'Serial number') ]]
 	then
 		true | dmenu -p 'Insert smartcard and press enter...'
+		if [[ ! $(gpg --card-status | grep 'Serial number') ]]
+		then
+			exit
+		fi
 	fi
 	passcode=$(true | dmenu -p "Input smartcard passcode")
 }
