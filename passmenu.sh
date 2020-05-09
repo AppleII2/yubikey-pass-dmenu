9392f7e465efed5b38859bb8486c69b698e9c774
Nelson Healy <nelson@atom.industries>
2020-05-08 20:29:11 -0400

    changed generation of password list to allow for passwords in all layers of folder subtree

--- passmenu.sh
+++ passmenu.sh
@@ -10,20 +10,18 @@ fi
 path=$HOME/.password-store # Location of password store file
 
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
-password_files=( "$prefix"/**/*.gpg )
-password_files=( "${password_files[@]#"$prefix"/}" )
-password_files=( "${password_files[@]%.gpg}" )
 
-passname=$(printf '%s\n' "${password_files[@]}" | dmenu "$@")
-
-[[ -n $passname ]] || exit
+passname=$(find $prefix -name '*.gpg' -printf '%f\n' | sed 's/\.gpg$//1' | dmenu)
+passpath=$(find $prefix -name $passname.gpg)
+[[ -n $passpath ]] || exit
 
 function get_pass {
+
 # If card present + unlocked or has had incorrect PIN tried
 if [[ $(gpg-connect-agent 'scd getinfo card_list' /bye) ]]
 then
 	# Try to get password without providing a passcode
-	password=$(gpg --batch --pinentry-mode loopback -d $path/"$passname.gpg" | cut -d$'\n' -f $req_field)
+	password=$(gpg --batch --pinentry-mode loopback -d $passpath | cut -d$'\n' -f $req_field)
 	[[ -z $password ]] || return
 fi
 # Prompt for smartcard if needed + get pin
@@ -31,7 +29,7 @@ fi
 [[ $(gpg --card-status) ]] || exit
 passcode=$(true | dmenu -p "Input smartcard PIN...")
 # Try to get password with provided PIN 
-password=$(gpg --batch --pinentry-mode loopback --passphrase $passcode -d $path/"$passname.gpg" | cut -d$'\n' -f $req_field)
+password=$(gpg --batch --pinentry-mode loopback --passphrase $passcode -d $passpath | cut -d$'\n' -f $req_field)
 }
 
 get_pass
