9d018485f3cbb5a56d43b1cfeb7bf645f632c6cc
Nelson Healy <nelson@atom.industries>
2020-05-10 23:51:49 -0400

    fixed bug - not working with passwords that have space in name

--- passmenu.sh
+++ passmenu.sh
@@ -9,7 +9,7 @@ fi
 
 prefix=${PASSWORD_STORE_DIR-~/.password-store}
 passname=$(find $prefix -name '*.gpg' -printf '%f\n' | sed 's/\.gpg$//1' | dmenu)
-passpath=$(find $prefix -name $passname.gpg)
+passpath=$(find $prefix -name "$passname".gpg)
 [[ -n $passpath ]] || exit
 
 function get_pass {
@@ -17,7 +17,7 @@ function get_pass {
 if [[ $(gpg-connect-agent 'scd getinfo card_list' /bye) ]]
 then
 	# Try to get password without providing a passcode
-	password=$(gpg --batch --pinentry-mode loopback -d $passpath | cut -d$'\n' -f $req_field)
+	password=$(gpg --batch --pinentry-mode loopback -d "$passpath" | cut -d$'\n' -f $req_field)
 	[[ -z $password ]] || return
 fi
 # Prompt for smartcard if needed + get pin
@@ -25,9 +25,9 @@ fi
 [[ $(gpg --card-status) ]] || exit
 passcode=$(true | dmenu -p "Input smartcard PIN...")
 # Try to get password with provided PIN 
-password=$(gpg --batch --pinentry-mode loopback --passphrase $passcode -d $passpath | cut -d$'\n' -f $req_field)
+password=$(gpg --batch --pinentry-mode loopback --passphrase "$passcode" -d "$passpath" | cut -d$'\n' -f $req_field)
 }
 
 get_pass
 [[ -n $password ]] || exit
-xdotool type --clearmodifiers $password 
+xdotool type --clearmodifiers "$password"
